daa99783f2bb596f5cd0fee74de801ba41d08cdd
Peter Rifkind <prifkind@gmail.com>
2021-12-15 22:55:47 -0500

    Aeed video player, added Semantic UI grid, additional styling of components

--- src/Components/VideoItem.js
+++ src/Components/VideoItem.js
@@ -1,10 +1,15 @@
 import "./VideoItem.css";
 import React from "react";
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div className="video-item item">
-      <img src={video.snippet.thumbnails.medium.url} className="ui image" />
+    <div
+      className="video-item item"
+      onClick={() => {
+        onVideoSelect(video);
+      }}
+    >
+      <img src={video.snippet.thumbnails.medium.url} className="ui image" alt={video.snippet.title}/>
       <div className="content">
         <div className="header">{video.snippet.title}</div>
       </div>
--- src/Components/VideoList.js
+++ src/Components/VideoList.js
@@ -1,9 +1,14 @@
 import React from "react";
 import VideoItem from "./VideoItem";
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map((v) => {
-    return <VideoItem video={v}/>;
+    return (
+      <VideoItem key={v.id.videoId}
+        video={v}
+        onVideoSelect={onVideoSelect}
+      />
+    );
   });
 
   return <div className="ui relaxed divided list">{renderedList}</div>;
